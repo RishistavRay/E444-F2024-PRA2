c4950079d16ffda3b2ca89699590fff288868255
Rishistav <rishistav@gmail.com>
2024-09-24 13:43:36 -0400

    example3

--- app.py
+++ app.py
@@ -23,8 +23,3 @@ def internal_server_error(e):
 def index():
     return render_template('index.html',
                            current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
